ec2768e6c4c6e07b8446dd46c2937b0b79c07343
Josue <josue@zipcodersmbp10.lan>
2024-07-09 20:54:08 -0400

    Finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,36 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int counterUp = 0;
+        int counterDown = 0;
+
+        for (int i = startIndex; i <= playList.length; i++){
+            if (i == playList.length){
+                i = 0;
+            }
+            if (playList[i].equals(selection)){
+                break;
+            }
+            counterUp++;
+        }
+        for (int i = startIndex; i >= 0; i--){
+
+            if (playList[i].equals(selection)){
+                break;
+            }
+            if (i == 0){
+                i = playList.length;
+            }
+            counterDown++;
+
+        }
+        if (counterUp < counterDown){
+                return counterUp;
+            } else if (counterUp == counterDown){
+                return counterUp;
+            } else {
+                return counterDown;
+            }
+
     }
 }
